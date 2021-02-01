bbbdd775138894ae081427cfd824a11f9610dd6a
Marcelo M. Rocha <mmrocha@ufrgs.br>
2021-02-01 13:10:43 -0300

    MPU6050

--- Arduino/ReadMPU6050/ReadMPU6050.ino
+++ Arduino/ReadMPU6050/ReadMPU6050.ino
@@ -6,8 +6,8 @@
 #include <SPI.h>
 
 // Auxiliary variables
-   char     line[68];           // formatted output
-   long     t0, t;              // time from internal clock
+   char     line[30];             // formatted output
+   long     t0, t;                // time from internal clock
 
 // MPU6050
    int      MPU = 0x68;           // MPU address (AD0 HIGH)
@@ -22,11 +22,11 @@ void setup() {
    Serial.begin(115200);
    while (!Serial)delay(100);
 
-   Wire.begin();                      // Initialize comunication
-   Wire.beginTransmission(MPU);       // Start communication with MPU6050 // MPU=0x68
-   Wire.write(0x6B);                  // Talk to the register 6B
-   Wire.write(0x00);                  // Make reset - place a 0 into the 6B register
-   Wire.endTransmission(true);        // end the transmission
+   Wire.begin();                 // Initialize comunication
+   Wire.beginTransmission(MPU);  // Start communication with MPU6050
+   Wire.write(0x6B);             // Talk to the register 6B
+   Wire.write(0x00);             // Make reset(0 into the 6B register)
+   Wire.endTransmission(true);   // end the transmission
    
    delay(1000);
    t0 = millis();
@@ -46,22 +46,22 @@ void loop(){
               Wire.beginTransmission(MPU);
               Wire.write(0x3B);
               Wire.endTransmission(false);
-              Wire.requestFrom(MPU, 14, true);
+              Wire.requestFrom(MPU, 6, true);  // 2 bytes for each parameters
 
               AcX  = Wire.read()<<8 | Wire.read();
               AcY  = Wire.read()<<8 | Wire.read();
               AcZ  = Wire.read()<<8 | Wire.read();
-              tmp  = Wire.read()<<8 | Wire.read();
-              GyX  = Wire.read()<<8 | Wire.read();
-              GyY  = Wire.read()<<8 | Wire.read();
-              GyZ  = Wire.read()<<8 | Wire.read();
+//            tmp  = Wire.read()<<8 | Wire.read();
+//            GyX  = Wire.read()<<8 | Wire.read();
+//            GyY  = Wire.read()<<8 | Wire.read();
+//            GyZ  = Wire.read()<<8 | Wire.read();
 
               t  = millis() - t0;
 
-              sprintf(line,"%9ld  %6d  %6d  %6d  %6d  %6d  %6d  %6d\n", 
-                             t,    AcX, AcX, AcZ, tmp, GyX, GyY, GyZ);
+              sprintf(line,"%9ld %6d %6d %6d", 
+                             t,   AcX,AcY,AcZ);
 
-              Serial.print(line);
+              Serial.println(line);
               Wire.endTransmission(true); 
               delayMicroseconds(1000);
               }
