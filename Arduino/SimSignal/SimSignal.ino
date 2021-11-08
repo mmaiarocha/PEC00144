67833e2d6d58d8353278408a83bd176b5b7602df
Marcelo M. Rocha <mmrocha@ufrgs.br>
2021-11-08 19:34:42 -0300

    in class

--- Arduino/SimSignal/SimSignal.ino
+++ Arduino/SimSignal/SimSignal.ino
@@ -1,37 +1,43 @@
 int  led = 13;
+long t0;
 
 void setup(){
-     Serial.begin(9600);
-     
+     Serial.begin(115200);
+
      pinMode(led, OUTPUT);
      digitalWrite(led, LOW);
       
      randomSeed(0);
      delay(100);
+     t0 = millis();
 }
 
 void loop(){
 
      long t;
      long data[3];
-     char line[40];
+     char line[41];
      byte gotIt;   
 
-     if (Serial.available() > 0){
-         gotIt   = Serial.read();
+     if (Serial.available()){
+         String NS = Serial.readString();
+         int    N  = NS.toInt();
          digitalWrite(led, HIGH);
+         
+         for (int i = 0; i < N; i++){
 
-         t       = millis();
-         data[0] = random(99999999);
-         data[1] = random(99999999);
-         data[2] = random(99999999);
-     
-         sprintf(line,   "%9ld %9ld %9ld %9ld", 
-                 t, data[0], data[1], data[2]);
-                        
-         Serial.println(line);
-
-         delay(50);
+              t       = millis() - t0;
+              data[0] = random(99999999);
+              data[1] = random(99999999);
+              data[2] = random(99999999);
+       
+              sprintf(line, "%9ld %9ld %9ld %9ld\n", 
+                       t, data[0], data[1], data[2]);
+                          
+              Serial.print(line);
+              delayMicroseconds(1000);
+              }
+              
          digitalWrite(led, LOW); 
-         }
+     }
 }
